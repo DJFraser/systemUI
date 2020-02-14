edf82e50b82133e0f781f0704c5608e41365bb1c
Duncan Fraser <duncanjohn6@gmail.com>
2020-02-14 18:41:15 +0000

    Baisc Functionality Added
    Added CSS. Initial js functionality added

--- views/assets/js/main.mjs
+++ views/assets/js/main.mjs
@@ -1,8 +1,5 @@
 const baseUrl = window.location.origin;
 
-let systemList = [];
-
-
 $(document).ready(() => {
 
     // Set Page Title
@@ -17,9 +14,95 @@ $(document).ready(() => {
     $.ajax({
         url : baseUrl + '/api/getSystems',
         success : (data) => {
-            systemList = data;
-
-            console.log(systemList)
+            createCards(data);
         }
     })
 })
+
+function createCards(data){
+    data.forEach((system, index) => {
+        // Create System Cards
+        $.ajax({
+            url : baseUrl + '/api/getSystems',
+            method : "POST",
+            data: {
+                "system" : system
+            },
+            success : (data) => {
+                console.log(data);
+
+                systemHTML(data.type, data.name, index);
+                serviceHTML(data.services, index)
+            }
+        })
+    })
+}
+
+/**
+ * create system card
+ */
+function systemHTML(type, name, index){
+    let str = '';
+
+    str = `
+    <div id="system_${index}" class="system ${type}">
+        <div class="system_header">
+            <table class="system_table">
+                <tr>
+                    <td class="system_image">${systemImage(type)}</tD>
+                    <th class="system_name">${name}</th>
+                </tr>
+            </table>
+        </div>
+        <div class="services" id="service_${index}"></div>
+    </div>`
+
+    $('#systems').append(str)
+}
+
+/**
+ * create service card
+ */
+function serviceHTML(services, index){
+    let str = '';
+
+
+    services.forEach((service) => {
+        let dest = '';
+        if(service.hasOwnProperty('port')){
+            dest = `:${service.port}`
+        }
+
+        str += `
+        <div class="service">
+            ${service.service} <a href="http://192.168.0.30${dest}" target="_blank">link</a>
+        </div>`
+    })
+
+    $(`#service_${index}`).append(str)
+}
+
+/**
+ * Convert System Type to Font Awesome Image
+ * @param type System Type
+ */
+function systemImage(type){
+    let imageHTML = ''
+    
+    switch (type){
+        case "media" : {
+            imageHTML = `<i class="fas fa-play-circle"></i>`;
+            break;
+        }
+        case "server" : {
+            imageHTML = `<i class="fas fa-server"></i>`;
+            break;
+        }
+        default : {
+            imageHTML = `<i class="fas fa-desktop"></i>`;
+            break;
+        }
+    }
+
+    return imageHTML;
+}

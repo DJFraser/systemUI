07cf5273d1eb08b7029d4c07ef57bc0a7085509f
Duncan Fraser <duncanjohn6@gmail.com>
2020-02-25 21:11:58 +0000

    Back to top button added to editor

--- views/assets/js/editor.mjs
+++ views/assets/js/editor.mjs
@@ -3,6 +3,14 @@ import * as card from './cards.mjs'
 const baseUrl = window.location.origin;
 
 $(document).ready(() => {
+    // Back to top button functionality  
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
     // scroll body to 0px on click
     $('#back-to-top').click(function () {
         $('body,html').animate({
